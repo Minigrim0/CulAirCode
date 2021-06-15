adfa2f2321c9bcd746cf4556d5576ac7291bd3cd
Minigrim0 <grimauflorent@gmail.com>
2021-06-15 17:02:23 +0200

    [Fix] ascpect ratio

--- src/video.py
+++ src/video.py
@@ -1,6 +1,5 @@
-import time
-
 import cv2
+import numpy as np
 from ffpyplayer.player import MediaPlayer
 from moviepy.editor import VideoFileClip
 
@@ -8,17 +7,29 @@ from moviepy.editor import VideoFileClip
 class Video:
     def __init__(self, path, queueSize=1024):
         self.stream = VideoFileClip(path).rotate(90)
-        cv2.setWindowProperty("frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-        self.player = MediaPlayer(path)
+        self.path = path
+        cv2.setWindowProperty(
+            "frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
         self.sleep_ms = 1 / self.stream.fps
         self.current_frame = 0
         self.time = 0
 
+        self.back = np.zeros((1080, 1920, 3), np.uint8)
+        self.back[:, :] = (0, 0, 0)
+
     def draw(self, timeElapsed):
+        if self.time == 0:
+            self.player = MediaPlayer(self.path)
         self.time += timeElapsed
-        frame = self.stream.get_frame(self.time)
+        frame = cv2.resize(self.stream.get_frame(self.time), (608, 1080))
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        cv2.imshow("frame", frame)
+
+        height, width = frame.shape[:2]
+        new_image = self.back
+        x_offset = 656
+        new_image[:, x_offset:x_offset + width] = frame
+
+        cv2.imshow("frame", new_image)
 
         audio_frame, val = self.player.get_frame()
         if val != "eof" and audio_frame is not None:
