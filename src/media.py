06bcc91ca4a054fd83e3a86ccf9604aee304a66d
Minigrim0 <grimauflorent@gmail.com>
2021-06-15 15:31:45 +0200

    [Upd] media

--- src/media.py
+++ src/media.py
@@ -2,6 +2,7 @@ import os
 import json
 
 from src.video import Video
+from src.text import Text
 
 
 class MediaModel:
@@ -20,22 +21,33 @@ class MediaModel:
             MediaModel.instance = self
         self.media_running = False
         self.currentMedia = None
+        self.folder = None
+
+    def isStillSame(self, folder):
+        return self.folder == folder
+
+    def mediaLoaded(self):
+        return self.currentMedia is not None
 
     def loadFolder(self, folder):
+        self.kill()
         with open(os.path.join(folder, "settings.json")) as settings_file:
             settings = json.load(settings_file)
 
+        self.folder = folder
         if settings["type"] == "video":
             self.currentMedia = Video(os.path.join(folder, settings["filename"]))
+        elif settings["type"] == "text":
+            self.currentMedia = Text(music=os.path.join(folder, settings["music"]), text=os.path.join(folder, settings["text"]))
 
-    def update(self):
+    def kill(self):
+        self.folder = None
         if self.currentMedia is not None:
-            if not self.currentMedia.update():
-                self.currentMedia.destroy()
-                self.currentMedia = None
-        return self.currentMedia is not None
+            self.currentMedia.destroy()
+        self.currentMedia = None
 
-    def limitFrameRate(self, timeElapsed):
+    def update(self, timeElapsed):
         if self.currentMedia is not None:
-            self.currentMedia.limitFrameRate(timeElapsed)
+            if not self.currentMedia.draw(timeElapsed):
+                self.kill()
         return self.currentMedia is not None
