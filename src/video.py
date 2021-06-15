b751badf48fab003dfc685fd75d1e05014acfdcd
Minigrim0 <grimauflorent@gmail.com>
2021-06-15 15:31:08 +0200

    [Upd] Video

--- src/video.py
+++ src/video.py
@@ -2,29 +2,28 @@ import time
 
 import cv2
 from ffpyplayer.player import MediaPlayer
+from moviepy.editor import VideoFileClip
 
 
 class Video:
-    def __init__(self, path):
+    def __init__(self, path, queueSize=1024):
+        self.stream = VideoFileClip(path).rotate(90)
+        cv2.setWindowProperty("frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
         self.player = MediaPlayer(path)
-        self.video = cv2.VideoCapture(path)
-        fps = self.video.get(cv2.CAP_PROP_FPS)
-        self.sleep_ms = 1 / fps
-        self.grabbed = True
+        self.sleep_ms = 1 / self.stream.fps
+        self.current_frame = 0
+        self.time = 0
 
-    def update(self):
-        grabbed, frame = self.video.read()
-        audio_frame, val = self.player.get_frame()
-        if not grabbed:
-            return False
-
+    def draw(self, timeElapsed):
+        self.time += timeElapsed
+        frame = self.stream.get_frame(self.time)
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         cv2.imshow("frame", frame)
+
+        audio_frame, val = self.player.get_frame()
         if val != "eof" and audio_frame is not None:
             img, t = audio_frame
-        return True
-
-    def limitFrameRate(self, timeElapsed):
-        time.sleep(max(self.sleep_ms - timeElapsed, 0))
+        return self.time < self.stream.duration
 
     def destroy(self):
-        self.video.release()
+        pass
